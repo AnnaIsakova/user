4ac922b42d0c776a4e9028bfb2158ee3c3ae0af0
AnnaIsakova <tamgdepesok666@gmail.com>
2019-04-11 00:07:32 +0300

    +

--- src/main/java/me/annaisakova/user/controller/UserController.java
+++ src/main/java/me/annaisakova/user/controller/UserController.java
@@ -1,4 +1,25 @@
 package me.annaisakova.user.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import me.annaisakova.user.client.BookingClient;
+import me.annaisakova.user.model.Booking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1")
 public class UserController {
+
+    private final BookingClient bookingClient;
+
+    @GetMapping("/users/{userId}/booking")
+    public List<Booking> getBookings(@PathVariable Long userId) {
+        return bookingClient.getBookings(userId);
+    }
 }
